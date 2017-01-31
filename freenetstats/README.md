64750f1d0107d67c040fa3ff709f5d409a761ce3
server <facumo.fm@gmail.com>
2017-01-31 02:16:28 -0300

    Add script for parsing Freenet downloads and statistics page

---
+++ freenetstats/README.md
@@ -0,0 +1,17 @@
+#Freenet stats
+
+This bash script is able to parse your Freenet node's statistics page and show you your input and output rates. Optionally, you can see an scrolling list of your node's downloads list. After stopping the script by keystroke, it'll safely cleanup the temp file from your hard drive.
+
+Required packages:
+
+wget bash
+
+Usage: 
+
+freenetstats.sh ip:port [repeats/active] [interval]
+
+repeats: type an amount to repeat the script.
+   freenetstats.sh 127.0.0.1:8888 5 - repeats five times
+active: keep updating every one second or add interval.
+   freenetstats.sh active - update every one second.
+   freenetstats.sh active 5 - update every five seconds.
---
+++ freenetstats/freenetstats.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+####################################################################
+## ===> Script author: Facundo Montero (facumo.fm@gmail.com) <=== ##
+####################################################################
+COLUMNS=`tput cols`
+COLSFOREND=$(($COLUMNS - 15))
+LINES=`tput lines`
+LINESLIST=$(($LINES - 16))
+URISCRIPT="$1/stats/"
+URIDLLIST="$1/downloads/listKeys.txt"
+STATCOUNT=0
+trap ctrl_c INT
+function ctrl_c() {
+ echo "Performing safe cleanup for 'downloadstemp'..."
+ DLSIZE=`du -b "downloadstemp" | cut -f1`
+ dd if=/dev/zero of=./downloadstemp bs=1 count=$DLSIZE status=none
+ STATCOUNT=$(($STATCOUNT + $?))
+ rm downloadstemp
+ STATCOUNT=$(($STATCOUNT + $?))
+ if [ $STATCOUNT -eq 0 ]
+ then
+  clear
+  nicedraw
+  echo "Cleanup completed!"
+  nicedraw
+  exit 0
+ else
+  clear
+  nicedraw
+  echo "Something went wrong (permissions problem?). You should try deleting the file 'downloadstemp' by yourself, for safety reasons."
+  nicedraw
+  exit 1
+ fi
+}
+function nicedraw {
+ CHARAM=0
+ while [ $CHARAM -lt $COLUMNS ]
+ do
+  printf "="
+  CHARAM=$(($CHARAM + 1))
+ done
+ CHARAM=0
+}
+function printbottom {
+ CHARAM=0
+ while [ $CHARAM -lt $LINES ]
+ do
+  printf "\n"
+  CHARAM=$(($CHARAM + 1))
+ done
+ CHARAM=0
+}
+function updatestats {
+ printf "\n\nInput rate: \n\n"
+ wget -qO - $URISCRIPT | grep "Input Rate" | sed 's/&nbsp;/ /g' | sed 's/.*Rate: //' | sed 's/s).*/s)/'
+ printf "\n"
+ nicedraw
+ printf "\n\nOutput rate: \n\n"
+ wget -qO - $URISCRIPT | grep "Output Rate" | sed 's/&nbsp;/ /g' | sed 's/.*Rate: //' | sed 's/s).*/s)/'
+ printf "\n"
+ nicedraw
+}
+function getprints {
+ PRINTS=`cat $0 | grep "\n" | wc -l`
+}
+function processdllist {
+ wget -qO - $URIDLLIST | sed 's-.*/--g' | sed 's/%20/ /g' > downloadstemp
+ CURLINE=`cat downloadstemp | wc -l`
+ CURLINES=$CURLINE
+}
+function script {
+ clear
+ nicedraw
+ updatestats
+}
+function scriptactive {
+ CURLINE=$(($CURLINE - 1))
+ cat downloadstemp | head -$CURLINE | tail -$LINESLIST
+ if [ $CURLINE = 0 ]
+ then
+  CURLINE=$CURLINES
+ fi
+}
+getprints
+LINES=$(($LINES - $PRINTS))
+if [ -z $1 ]
+then
+ printf "Usage: \n\n$0 ip:port [repeats/active] [interval]\n\nrepeats: type an amount to repeat the script.\n   $0 127.0.0.1:8888 5 - repeats five times\nactive: keep updating every one second or add interval.\n   $0 active - update every one second.\n   $0 active 5 - update every five seconds.\n"
+else
+ if [ -z $2 ]
+ then
+  script
+  printbottom;nicedraw
+ else
+  if [ "$2" != "active" ]
+  then
+   COUNTSCRIPTFNET=$2
+   while [ $COUNTSCRIPTFNET -gt 0 ]
+   do
+    COUNTSCRIPTFNET=$(($COUNTSCRIPTFNET - 1))
+    script
+    printbottom;nicedraw
+    sleep 1
+   done
+  else
+   processdllist
+   while true
+   do
+    script
+    printf "\n"
+    printf "\n"
+    scriptactive
+    CURCOLS=0
+    while [ $CURCOLS -lt $COLSFOREND ]
+    do
+     printf " "
+     CURCOLS=$(($CURCOLS + 1))
+    done
+    CURLINEPRINT=$(($CURLINES - $CURLINE))
+    printf "($CURLINEPRINT / $CURLINES)"
+    printf "\n"
+    nicedraw
+    if [ -z $3 ]
+    then
+     sleep 1
+    else
+     sleep $3
+    fi
+   done
+  fi
+ fi
+fi
