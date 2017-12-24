1d60c75d63e536d15c05897d28a8fcbce0185631
FacuM <facumo.fm@gmail.com>
2017-12-24 16:05:47 -0300

    It should now be stable! See more...
    - Improve performance.
    - Get rid of heavy conditionals.
    - Only run if charger is plugged.
    - Custom triggers support.
    - Comment out all code blocks.
    - Correct syntax for printf sentences.

--- harpia/ledctl.sh
+++ harpia/ledctl.sh
@@ -22,68 +22,88 @@ then
    mist='minute'
   fi
   echo "Hey there! Turn off your screen and wait $interval $mist, then, the LED will start blinking."
-  #Convert minutes to seconds
+  # Convert minutes to seconds
   interval=$(($interval * 60))
+  # Check for custom trigger
+  if [ -z "$trigger" ]
+  then
+   echo "The custom trigger wasn't set, so I'll fall back to defaults (heartbeat)"
+   trigger="heartbeat"
+  fi
   # Blink the LED every defined minutes
   PREV='None'
   while true
   do
    sleep $interval
-   # Check if screen is on
-   service call power 12 | grep 1 > /dev/null
-   if [ $? -ne 0 ] || [ $son ]
+   # Run ONLY if charger is present
+   if [ "$(cat /sys/class/power_supply/battery/status)" == 'Charging' ]
    then
-    # Get DB entries
-    QUERY="SELECT * FROM log"
-    if [ $debug == 1 ]
+    if [ $debug -eq 1 ]
     then
-     echo "QUERY: $QUERY"
+     echo 'CHARGER: Plugged.'
     fi
-    CMD="sqlite3 /data/system/notification_log.db '$QUERY'"
-    if [ $debug == 1 ]
+    # Check if screen is on
+    service call power 12 | grep 1 > /dev/null
+    if [ $? -ne 0 ] || [ $son -eq 1 ]
     then
-     echo "COMMAND: $CMD"
+     # Get DB entries
+     QUERY="SELECT * FROM log"
+     if [ $debug -eq 1 ]
+     then
+      echo "QUERY: $QUERY"
+     fi
+     # Prepare the command
+     CMD="sqlite3 /data/system/notification_log.db '$QUERY'"
+     if [ $debug -eq 1 ]
+     then
+      echo "COMMAND: $CMD"
+     fi
+    # Get last notification
+    LNOF=$(su -c "$CMD | tail -1")
+    # Get last notification ID
+    LID=$(printf "$LNOF" | cut -d '|' -f 7)
+    if [ $debug -eq 1 ]
+    then
+     echo "LAST NOTIFICATION: $LNOF"
+     echo "LAST ID: $LID"
     fi
-   # Get last notification
-   LNOF=$(su -c "$CMD | tail -1")
-   # Get last notification ID
-   LID=$(printf $LNOF | cut -d '|' -f 7)
-   if [ $debug == 1 ]
-   then
-    echo "LAST NOTIFICATION: $LNOF"
-    echo "LAST ID: $LID"
-   fi
-   if [ "$LNOF" != "$PREV" ] && [ "$LID" != "$PID" ]
-   then
-      # On
-      su -c 'echo heartbeat > /sys/class/leds/charging/trigger'
-      if [ $debug == 1 ]
-      then
-       cat /sys/class/leds/charging/trigger
-      fi
-      sleep  3
-      # Off
-      su -c 'echo none > /sys/class/leds/charging/trigger'
-      if [ $debug == 1 ]
-      then
-       cat /sys/class/leds/charging/trigger
-      fi
-      # Save previous notification
-      PREV=$LNOF
-      # Save previous ID to skip execution if equals
-      PID=$(printf $LNOF | cut -d '|' -f 7)
-      if [ $debug == 1 ]
-      then
-       echo "PREVIOUS NOTIFICATION: $PREV"
-       echo "PREVIOUS ID: $PID"
-      fi
+    if [ "$LNOF" != "$PREV" ] && [ "$LID" != "$PID" ]
+    then
+       # On
+       su -c 'echo heartbeat > /sys/class/leds/charging/trigger'
+       if [ $debug -eq 1 ]
+       then
+        cat /sys/class/leds/charging/trigger
+       fi
+       sleep  3
+       # Off
+       su -c 'echo none > /sys/class/leds/charging/trigger'
+       if [ $debug -eq 1 ]
+       then
+        cat /sys/class/leds/charging/trigger
+       fi
+       # Save previous notification
+       PREV=$LNOF
+       # Save previous ID to skip execution if equals
+       PID=$(printf "$LNOF" | cut -d '|' -f 7)
+       if [ $debug -eq 1 ]
+       then
+        echo "PREVIOUS NOTIFICATION: $PREV"
+        echo "PREVIOUS ID: $PID"
+       fi
+     fi
     fi
+    else
+     if [ $debug -eq 1 ]
+     then
+      echo 'CHARGER: Not plugged.'
+     fi
    fi
   done
  fi
 else
  # If it isn't, just generate the file and exit with a generic error code.
- printf '#!/system/bin/bash\n# Set to 0 to disable, 1 to enable\nenabled=1\n# Set the reading interval: (minutes)\ninterval=10\n# Also with screen on (could cause issues)\nson=0\n#Toggle debugging\ndebug=0' > /sdcard/ledsettings.sh
+ printf '#!/system/bin/bash\n# Set to 0 to disable, 1 to enable\nenabled=1\n# Set the reading interval: (minutes)\ninterval=10\n# Also with screen on (could cause issues)\nson=0\n# Custom LED trigger \ntrigger=""\n# Toggle debugging\ndebug=0' > /sdcard/ledsettings.sh
  printf '==========\nPLEASE READ\n==========\n\nUnable to startup, please edit /sdcard/ledsettings.sh before continuing or run again for defaults.\n'
  exit 1
 fi
