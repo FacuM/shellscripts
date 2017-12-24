d2b84330b2c446484a765c938afd85ad28bf41ae
FacuM <facumo.fm@gmail.com>
2017-12-24 16:14:32 -0300

    harpia: ledctl: update to v1.9, add the flashables.

---
+++ harpia/ledctl/ledsettings.sh
@@ -0,0 +1,9 @@
+#!/system/bin/bash
+# Set to 0 to disable, 1 to enable
+enabled=1
+# Set the reading interval: (minutes)
+interval=10
+# Also with screen on (could cause issues)
+son=0
+#Toggle debugging
+debug=0
