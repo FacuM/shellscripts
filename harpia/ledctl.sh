fd401eacc70515fbe882178381052be6be2ec7b3
FacuM <facumo.fm@gmail.com>
2017-12-24 12:47:18 -0300

    harpia: fix the timeout bug, add more verbosity if debugging

--- harpia/ledctl.sh
+++ harpia/ledctl.sh
@@ -10,12 +10,10 @@
 # Test whether everything is set up or not.
 if [ -e /sdcard/ledsettings.sh ]
 then
- # If it is, try. to use it
+ # If it is, try to use it
  source /sdcard/ledsettings.sh
  if [ $enabled -eq 1 ]
  then
-  # Convert hours to milliseconds
-  thold=$(($thold * 3600000))
   # Hello
   if [ $interval -gt 1 ]
   then
@@ -30,14 +28,13 @@ then
   PREV='None'
   while true
   do
-   sleep 1
-   #sleep $interval
+   sleep $interval
    # Check if screen is on
    service call power 12 | grep 1 > /dev/null
    if [ $? -ne 0 ] || [ $son ]
    then
     # Get DB entries
-    QUERY="SELECT * FROM log WHERE posttime_ms < $thold ORDER BY posttime_ms DESC LIMIT 1"
+    QUERY="SELECT * FROM log"
     if [ $debug == 1 ]
     then
      echo "QUERY: $QUERY"
@@ -47,13 +44,15 @@ then
     then
      echo "COMMAND: $CMD"
     fi
-    LNOF=$(su -c "$CMD")
-    if [ $debug == 1 ]
-    then
-     echo "LAST NOTIFICATION: $LNOF"
-    fi
+   # Get last notification
+   LNOF=$(su -c "$CMD | tail -1")
    # Get last notification ID
    LID=$(printf $LNOF | cut -d '|' -f 7)
+   if [ $debug == 1 ]
+   then
+    echo "LAST NOTIFICATION: $LNOF"
+    echo "LAST ID: $LID"
+   fi
    if [ "$LNOF" != "$PREV" ] && [ "$LID" != "$PID" ]
    then
       # On
@@ -69,16 +68,22 @@ then
       then
        cat /sys/class/leds/charging/trigger
       fi
-      # Save previous ID to skip execution if equals
+      # Save previous notification
       PREV=$LNOF
+      # Save previous ID to skip execution if equals
       PID=$(printf $LNOF | cut -d '|' -f 7)
+      if [ $debug == 1 ]
+      then
+       echo "PREVIOUS NOTIFICATION: $PREV"
+       echo "PREVIOUS ID: $PID"
+      fi
     fi
    fi
   done
  fi
 else
  # If it isn't, just generate the file and exit with a generic error code.
- printf '#!/system/bin/bash\n# Set to 0 to disable, 1 to enable\nenabled=1\n# Set this variable to the desired threshold: (hours)\nthold=1\n# Set the reading interval: (minutes)\ninterval=10\n# Also with screen on (could cause issues)\nson=0\n#Toggle debugging\ndebug=0' > /sdcard/ledsettings.sh
+ printf '#!/system/bin/bash\n# Set to 0 to disable, 1 to enable\nenabled=1\n# Set the reading interval: (minutes)\ninterval=10\n# Also with screen on (could cause issues)\nson=0\n#Toggle debugging\ndebug=0' > /sdcard/ledsettings.sh
  printf '==========\nPLEASE READ\n==========\n\nUnable to startup, please edit /sdcard/ledsettings.sh before continuing or run again for defaults.\n'
  exit 1
 fi
