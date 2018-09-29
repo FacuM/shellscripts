e68926eee966b79cae8e648d29e436fef9c158b1
Facundo Montero <facumo.fm@gmail.com>
2018-09-29 03:43:40 +0000

    shell: uploadtg: add optimized version of "uploadtg".

---
+++ shell/uploadtg/uploadtg.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+########################################
+#  Simple Telegram channel posting script
+########################################
+#
+#  Author: Facundo Montero <facumo.fm@gmail.com>
+#
+########################################
+#
+# Depends on: telegram-send, gdrive.
+#
+########################################
+
+# Function definition
+
+# testinst: reports an error if a required dependency can't be found.
+function testinst()
+{
+ if [ $1 == 127 ]
+ then
+  echo "Some dependencies haven't been met, please check the errors above."
+  exit 1
+ fi
+}
+
+# Main
+
+# Check dependencies
+telegram-send 2>&1 > /dev/null
+testinst $?
+gdrive 2>&1 > /dev/null
+testinst $?
+
+# Upload file and post in Telegram
+echo "Uploading ""$1""..."
+FID=$(gdrive upload $1 | cut -d ' ' -f 2 | head -2 | tail -1)
+echo "Sharing ""$1"" (""$FID"")""..."
+gdrive share $FID
+INFO=$(gdrive info $FID)
+telegram-send --format html '<b>============</b>'
+DOWNLOAD=$(printf "$INFO" | cut -d ' ' -f 2 | head -12 | tail -1)
+NAME=$(printf "$INFO" | cut -d ' ' -f 2 | head -2 | tail -1)
+telegram-send --format html '<b>FILE:</b> '"<a href=\"$DOWNLOAD\">"$NAME"</a>"
+MD5=$(printf "$INFO" | cut -d ' ' -f 2 | head -7 | tail -1)
+telegram-send --format html "<b>MD5:</b> <pre>""$MD5""</pre>"
+SHA256=$(sha256sum $1 | cut -d ' ' -f 1)
+telegram-send --format html "<b>SHA256:</b> <pre>""$SHA256""</pre>"
+if [ "$2" != '-u' ]
+then
+ telegram-send --format html '<b>NOTE:</b> '"$2"
+fi
+telegram-send --format html '<b>============</b>'
+STATUS=$?
+echo "NAME: "$NAME
+echo "MD5: "$MD5
+echo "DOWNLOAD: "$DOWNLOAD
+printf "STATUS: "
+if [ $STATUS -eq 0 ]
+then
+ printf "Posted."
+else
+ printf "Error."
+fi
+printf "\n"
+if [ "$2" == '-u' ] && [ "$2" != '' ]
+then
+ echo "FILE: "$1
+ telegram-send -f $1
+fi
+exit 0
+
