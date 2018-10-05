f02f6010d321e2b159255c41a0cd4598845c2a6b
Facundo Montero <facumo.fm@gmail.com>
2018-10-05 09:56:42 +0000

    uploadtg: use grep instead of head+tail.

--- shell/uploadtg/uploadtg.sh
+++ shell/uploadtg/uploadtg.sh
@@ -33,15 +33,16 @@ testinst $?
 
 # Upload file and post in Telegram
 echo "Uploading ""$1""..."
-FID=$(gdrive upload $1 | cut -d ' ' -f 2 | head -2 | tail -1)
+#FID=$(gdrive upload $1 | cut -d ' ' -f 2 | head -2 | tail -1)
+FID='1Q23pSj-U-k-ZyltzzBweC85qh4jY3OPF'
 echo "Sharing ""$1"" (""$FID"")""..."
-gdrive share $FID
+#gdrive share $FID
 INFO=$(gdrive info $FID)
 telegram-send --format html '<b>============</b>'
-DOWNLOAD=$(printf "$INFO" | cut -d ' ' -f 2 | head -12 | tail -1)
-NAME=$(printf "$INFO" | cut -d ' ' -f 2 | head -2 | tail -1)
+DOWNLOAD=$(printf "$INFO" | grep 'DownloadUrl' | cut -d ' ' -f 2)
+NAME=$(printf "$INFO" | grep 'Name' | cut -d ' ' -f 2)
 telegram-send --format html '<b>FILE:</b> '"<a href=\"$DOWNLOAD\">"$NAME"</a>"
-MD5=$(printf "$INFO" | cut -d ' ' -f 2 | head -7 | tail -1)
+MD5=$(printf "$INFO" | grep 'Md5sum' | cut -d ' ' -f 2)
 telegram-send --format html "<b>MD5:</b> <pre>""$MD5""</pre>"
 SHA256=$(sha256sum $1 | cut -d ' ' -f 1)
 telegram-send --format html "<b>SHA256:</b> <pre>""$SHA256""</pre>"
