62b7de67c56003cd18569055a03183e34184b0bc
Facundo Montero <facumo.fm@gmail.com>
2018-11-01 03:30:35 +0000

    sepolicy: make it more "user-friendly".

--- sepolicy/patchsepolicy.sh
+++ sepolicy/patchsepolicy.sh
@@ -7,6 +7,17 @@
 #
 ########################################
 
+if [ -z $1 ]
+then
+ printf "\nInvalid input.\n\nUsage: bash $0 path/to/audit2allowoutput\n"
+ exit 1
+else
+ if [ ! -f "$1" ]
+ then
+  echo "While reading \"$1\": file not found."
+  exit 1
+ fi
+fi
 unset OUT
 echo "- Patching sepolicies from \"$1\"..."
 while read -r line
