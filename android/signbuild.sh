58e8ae777f49663cb94df51327d55d10b8d8477e
Facundo Montero <facumo.fm@gmail.com>
2018-11-02 04:13:33 +0000

    android: signbuild.sh: now for all ROMs! Also write md5sum.

--- android/signbuild.sh
+++ android/signbuild.sh
@@ -4,14 +4,15 @@ then
  echo "You must provide a device codename."
  echo "Nothing to do."
 else
-D=$(date +%Y%m%d)
 echo "The build date is "$(date)"."
 echo "Set up the environment."
 . build/envsetup.sh
 echo "Will now run breakfast for device codename $1".
-breakfast $1 &&
+TARGETZIP=$(breakfast $1)
+TARGETZIP=$(echo "$TARGETZIP" | grep TARGET_PRODUCT | sed 's/TARGET_PRODUCT=//' | sed 's/_harpia//')-$(echo "$TARGETZIP" | grep LINEAGE_VERSION | sed 's/LINEAGE_VERSION=//').zip
+echo '==> Target filename is '"$TARGETZIP"
 echo "Making target files package..."
-mka target-files-package dist &&
+mka -j$(nproc --all) target-files-package dist &&
 printf "Signing apks... "
 TARGET=$(ls -tr1 out/dist | tail -1)
 printf "(target from $TARGET)\n"
@@ -23,5 +24,6 @@ echo "Making OTA package..."
 ./build/tools/releasetools/ota_from_target_files -k ~/.android-certs/releasekey \
     --block --backup=true \
     signed-target_files.zip \
-    'lineage-16.0-'"$D"'-UNOFFICIAL-'"$1"'.zip'
+    $TARGETZIP
+md5sum "$TARGETZIP" > "$TARGETZIP"'.md5sum'
 fi
