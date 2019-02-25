9a7d3d016e0ee67fadc327caa5ee9642dc961925
FacuM <facumo.fm@gmail.com>
2019-02-25 10:51:30 +0000

    android: prepenv: support Arch Linux too (WIP).

--- android/prepenv/prepenv.sh
+++ android/prepenv/prepenv.sh
@@ -11,7 +11,8 @@
 ########################################
 
 CCACHE_SIZE='100G'
-BUILD_DEPENDENCIES='bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev screen screenie tmux unzip libisl15'
+DEBIAN_BUILD_DEPENDENCIES='bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev screen screenie tmux unzip libisl15'
+ARCH_BUILD_DEPENDENCIES='bc bison curl libisl15 unzip zip tmux screen screenie lib32-gcc-libs git gnupg flex gperf sdl wxgtk2 squashfs-tools ncurses zlib schedtool perl-switch libxslt python2-virtualenv rsync ncurses5-compat-libs lib32-zlib lib32-ncurses lib32-readline lib32-ncurses5-compat-libs'
 BIN_PATH="$HOME"'/bin'
 # This script must be run from the source shell, if not, crash.
 if [ "${BASH_SOURCE[0]}" == "${0}" ]
@@ -25,14 +26,30 @@ Usage:
  exit 1
 fi
 
+OS_RELEASE="$(cat /etc/*-release)"
+if echo "$OS_RELEASE" | grep "Arch" > /dev/null
+then
+ PM_CMD='pacman -Syu --noconfirm'
+ sudo $PM_CMD base-devel
+ rm -rf yay
+ git clone https://aur.archlinux.org/yay.git
+ cd yay
+ makepkg -si --noconfirm
+ cd ../
+ PM_CMD='yay -Syu --noconfirm'
+ BUILD_DEPENDENCIES=$ARCH_BUILD_DEPENDENCIES
+else
+ PM_CMD='apt-get'
+ sudo $PM_CMD update
+ PM_CMD="$PM_CMD"' -y install'
+ BUILD_DEPENDENCIES=$DEBIAN_BUILD_DEPENDENCIES
+fi
 echo 'Please allow me to run as root.'
 sudo echo "--> We are now rooted!" || echo '--> Failed to acquire root permission, quitting.'
-echo '=> Updating apt cache...'
-sudo apt-get update
 if [ $? -eq 0 ]
 then
  echo '=> Installing dependencies...'
- sudo apt-get -y install $BUILD_DEPENDENCIES
+ sudo $PM_CMD $BUILD_DEPENDENCIES
  if [ $? -eq 0 ]
  then
   echo '=> Setting up your profile...'
@@ -83,6 +100,4 @@ PATH='"$BIN_PATH"':$PATH' >> ~/.bashrc
  else
   echo '=> Failed to write build settings to "~/.bashrc".'
  fi
-else
- echo '=> Failed to update package cache. Please try again.'
 fi
