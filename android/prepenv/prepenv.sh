ee6df0bd1d818800eeaffb0187289ef6bfcbb6b2
FacuM <facumo.fm@gmail.com>
2019-02-26 02:31:03 +0000

    android: prepenv: oops, isl should be isl15 instead.

--- android/prepenv/prepenv.sh
+++ android/prepenv/prepenv.sh
@@ -13,7 +13,7 @@
 CCACHE_SIZE='100G'
 DEBIAN_BUILD_DEPENDENCIES='bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev screen screenie tmux unzip libisl15 git-lfs'
 ARCH_BUILD_DEPENDENCIES='bc bison curl unzip zip tmux screen lib32-gcc-libs git gnupg flex gperf sdl wxgtk2 squashfs-tools lineageos-devel git-lfs'
-ARCH_BUILD_DEPENDENCIES_NOROOT='isl'
+ARCH_BUILD_DEPENDENCIES_NOROOT='isl15'
 BIN_PATH="$HOME"'/bin'
 # This script must be run from the source shell, if not, crash.
 if [ "${BASH_SOURCE[0]}" == "${0}" ]
