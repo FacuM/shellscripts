c04f40c165c8a9643ace81e6595a0465f59ca1a5
Developer Double A <developer.doublea@gmail.com>
2019-03-10 01:55:16 +0530

    Add a Google Drive CLI written in NodeJS

---
+++ node/Client.js
@@ -0,0 +1,70 @@
+const { promisify } = require('util');
+const { google: Google } = require('googleapis');
+const { getType } = require('mime');
+
+const Authenticator = require('./Authenticator');
+var { readFile, createReadStream } = require('fs');
+readFile = promisify(readFile);
+
+module.exports = class extends require('events').EventEmitter {
+	constructor({ clientID, clientSecret, redirectURL }) {
+		super();
+		this.client = new Google.auth.OAuth2(clientID, clientSecret, redirectURL);
+		this.initiate();
+	}
+
+	initiate() {
+		readFile('token.json').then(token => {
+			this.client.setCredentials(JSON.parse(token));
+			this.drive = Google.drive({ version: 'v3', auth: this.client });
+			this.emit('ready');
+		}).catch(() => {
+			Authenticator.authorize(this.client);
+		});
+	}
+
+	listFiles(pageSize = 10) {
+		this.drive.files.list({
+			pageSize,
+			fields: 'nextPageToken, files(id, name)'
+		}, (error, result) => {
+			if (error) return console.log(`The API returned an error: ${error}`);
+			const { files } = result.data;
+			if (files.length) {
+				console.log('Files:');
+				files.forEach(file => {
+					console.log(`${file.name} (${file.id})`);
+				});
+			} else console.log('No files found.');
+		});
+	}
+
+	uploadFile(file) {
+		this.drive.files.create({
+			resource: {
+				name: file
+			},
+			media: {
+				mimeType: getType(file),
+				body: createReadStream(file)
+			},
+			fields: 'id'
+		}, (error, { data }) => {
+			if (error) throw error;
+			console.log('File ID: ', data.id);
+		});
+	}
+
+	shareFile(fileId) {
+		this.drive.permissions.create({
+			fileId,
+			resource: {
+				role: 'reader',
+				type: 'anyone'
+			}
+		}, (error, { status }) => {
+			if (error || status !== 200) throw error;
+			console.info(`https://drive.google.com/uc?id=${fileId}&export=download`);
+		});
+	}
+};
---
+++ node/gdrive.js
@@ -0,0 +1,35 @@
+const Client = require('./Client');
+
+const drive = new Client(JSON.parse(require('fs').readFileSync('credentials.json')).drive);
+
+drive.on('ready', () => {
+	const [command, argument] = process.argv.slice(2);
+
+	switch (command) {
+		case 'help' || '-help' || '-h':
+			logHelpMessage();
+			break;
+		case 'upload' || '-upload' || '-u':
+			if (!argument) return console.error('Requires the file name.');
+			drive.uploadFile(argument);
+			break;
+		case 'list' || '-list' || '-l':
+			drive.listFiles();
+			break;
+		case 'share' || '-share' || '-s':
+			if (!argument) return console.error('Requires the file ID, use the list command.');
+			drive.shareFile(argument);
+			break;
+		default:
+			logHelpMessage(true);
+	}
+
+	function logHelpMessage(unknowCommand) {
+		console.log(`${unknowCommand ? 'Command not found.' : ''}
+  Here's the list of available commands:
+  upload: uploads a file
+  share: share the file, requires file ID
+  list: lists all the available files with their IDs`);
+	}
+});
+
