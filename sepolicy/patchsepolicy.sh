db29109d61144df3d2e8b77c0b5724a171202934
FacuM <facumo.fm@gmail.com>
2019-04-06 03:05:21 +0000

    sepolicy: allow to pass the full log! (logcat/dmesg)
    - Small performance improvements.
    - Add and improve startup validations.

--- sepolicy/patchsepolicy.sh
+++ sepolicy/patchsepolicy.sh
@@ -13,17 +13,42 @@ PRIVATE_TYPES=('adbtcp_prop' 'storaged' 'hal_allocator' 'sysinit' 'ctl_mdnd_prop
 
 if [ -z $1 ]
 then
- printf "\nInvalid input.\n\nUsage: bash $0 path/to/audit2allowoutput\n"
+ printf '\nInvalid input.\n\nUsage: bash '"$0"' path/to/log\n'
  exit 1
 else
  if [ ! -f "$1" ]
  then
   echo "While reading \"$1\": file not found."
   exit 1
+ else
+  if ! grep 'avc' $1 > /dev/null
+  then
+   echo 'No denials found.'
+   exit 1
+  else
+   if [ ! -d 'sepolicy' ]
+   then
+    printf '\nNot a device tree or missing the "sepolicy" directory. If this is the expected behavior please type the following commands and try again: \n\nmkdir sepolicy\nmkdir sepolicy_private\n'
+    exit 1
+   fi
+  fi
  fi
 fi
 unset OUT
 echo "- Patching sepolicies from \"$1\"..."
+audit2allow --help > /dev/null 2> /dev/null
+if [ $? -eq 127 ]
+then
+ printf '\naudit2allow is missing.\n\n\nBuild the ROM and add it to the path as follows: \n\n. build/envsetup.sh\nbreakfast device_codename\n'
+else
+ echo '- Parsing log (filtering denials)...'
+ cat "$1" | grep avc > tmp
+ echo '- Parse completed.'
+ echo '- Generating rules...'
+ audit2allow < tmp > sepolicyfix
+ echo '- Generation completed.'
+ rm -f tmp
+fi
 while read -r line
 do
  if printf "$line" | grep '#=============' 2> /dev/null > /dev/null
@@ -69,5 +94,8 @@ do
    fi
   fi
  fi
-done < $1
-printf '\n- Patch completed.\n'
+done < sepolicyfix
+printf '\n- Cleaning up...\n'
+rm -f sepolicyfix
+echo '- Cleanup completed.'
+echo '- Patch completed.'
